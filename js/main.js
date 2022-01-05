1ad75aa9cdb297203ded3e7e09bbce6f1d961042
malcolmlindberg <malcolm.lindberg@hotmail.se>
2022-01-05 12:36:25 +0100

    Class update

--- js/main.js
+++ js/main.js
@@ -1,51 +1,85 @@
-let todoInput = document.getElementById("todo-input");
-let todoBtn = document.getElementById("todo-btn");
-let todoContainer = document.getElementById("todo-container");
-let todoList = document.getElementById("todo-list");
-
-//EventListerners
-todoBtn.addEventListener("click", addTodo);
-todoList.addEventListener("click", deleteList);
-
-//Functions
-function addTodo(event){
-    event.preventDefault();
-    //div
-    let firstDiv = document.createElement("div");
-    firstDiv.classList.add("todo");  
-    // li 
-    let newLi = document.createElement("li");
-    newLi.innerHTML = todoInput.value;
-    newLi.classList.add("newlist-item");  
-    firstDiv.appendChild(newLi);  
-    //Button for complete
-    let completeButton = document.createElement("button");
-    completeButton.innerHTML = '<i class="far fa-check-circle"></i>';
-    completeButton.classList.add("btn-complete");
-    firstDiv.appendChild(completeButton);
-    //Button close
-    let closeButton = document.createElement("button");
-    closeButton.innerHTML = '<i class="fas fa-trash-alt"></i>';
-    closeButton.classList.add("btn-close");
-    firstDiv.appendChild(closeButton);
-    todoList.appendChild(firstDiv);
-    //Clear input value
-    todoInput.value = "";
-}
-//Delete todoitem
-function deleteList(e){
-    let item = e.target;
-    if (item.classList[0] === "btn-close"){
-        let todo = item.parentElement;
-        todo.remove();
+class Todo{
+    constructor(todo, ){
+        this.todoItem = todo;
+        this.done = false;
     }
-    //Completing 
-if(item.classList[0] === "btn-complete"){
-    let todo = item.parentElement;
-    todo.classList.toggle("completed");
-}
 }
 
+let todoList = [];
 
+//Hämta id
+let todoInput = document.getElementById("todoInput");
+let todoUl = document.getElementById("todoUl");
+let addBtn = document.getElementById("addBtn");
 
+window.onload = function(){
+  click();
+}
+
+function click(){
+  addBtn.addEventListener("click", addTodoBtn);
+  generateHtml();
+}
+
+// lägger till värdet från input i todoList + gör så sidan ej laddas om
+function addTodoBtn(event) {
+  event.preventDefault();
+  let todoItem = new Todo(todoInput.value);
+  todoList.push(todoItem);
+  console.log(todoList);
+  generateHtml();
+}
+
+//Generar lista vid knapptryck
+function generateHtml(){
+  let ul = document.getElementById("todoUl");
+  ul.innerHTML = "";
+  //Loopa
+  for (let i = 0; i < todoList.length; i++) {
+      //Skapa li/span
+      let todoLi = document.createElement("li");
+      todoLi.classList.add("todoLi");
+      let todoSpan = document.createElement("span");
+      todoSpan.innerText = todoList[i].todoItem;
+      todoSpan.classList.add("todoSpan");
+      todoLi.appendChild(todoSpan);
+  
+      //Markera klar knapp
+      let completeBtn = document.createElement("button");
+      completeBtn.className = "completeBtn";
+      completeBtn.innerHTML = '<i class="far fa-check-circle"></i>';
+      completeBtn.id = "completeBtn";
+      completeBtn.addEventListener("click", (event)=>{
+        event.preventDefault();
+        console.log("Klart knapp funkar");
+        todoList[i].done = !todoList[i].done;
+        generateHtml();
+      });
+      
+      //Ta bort knapp
+      let trashBtn = document.createElement("button");
+      trashBtn.innerHTML = '<i class="far fa-trash-alt"></i>';
+      trashBtn.className = "trashBtn";
+      trashBtn.addEventListener("click", (event)=>{
+        event.preventDefault();
+        todoList.splice(i, 1);
+        generateHtml();
+      });
+      
+      // //Append
+      todoLi.appendChild(completeBtn);
+      todoLi.appendChild(trashBtn);
+      ul.appendChild(todoLi);
+
+      //Lägger till text-decoration
+      if (todoList[i].done === true) {
+        todoSpan.classList.add("completeBtnStyle");
+      }
+    }
     
+    //Rensar
+    todoInput.value = "";
+  }
+  
+
+
